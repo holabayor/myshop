249cbf9415f2f1f9076e3b725a9e5a5c06117fdc
holabayor <liasu.olabayo@gmail.com>
2023-05-31 22:16:47 +0100

    bug fixed

--- orders/models.py
+++ orders/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
         return sum(item.get_cost() for item in self.items.all())
 
 
-class OrderItems(models.Model):
+class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
     product = models.ForeignKey(
         Product, related_name="order_items", on_delete=models.CASCADE
