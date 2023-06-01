13bbda013bc4bff1e31df09b7de3fc698a306316
holabayor <liasu.olabayo@gmail.com>
2023-06-01 21:13:13 +0100

    up next - celery :fire:

--- orders/models.py
+++ orders/models.py
@@ -10,8 +10,8 @@ class Order(models.Model):
     address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=False)
-    updated = models.DateTimeField(auto_now=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
 
     class Meta:
