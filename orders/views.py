e0443d9b736eade286a6abe732bf571f1c21598f
holabayor <liasu.olabayo@gmail.com>
2023-06-01 20:02:31 +0100

    update

--- orders/views.py
+++ orders/views.py
@@ -18,11 +18,9 @@ def order_create(request):
                     price=item["price"],
                     quantity=item["quantity"],
                 )
-                # clear the cart
-                cart.clear()
-                return render(request, "orders/order/created.html", {"order": order})
-            else:
-                form = OrderCreateForm()
-            return render(
-                request, "orders/order/create.html", {"cart": cart, "form": form}
-            )
+            # clear the cart
+            cart.clear()
+            return render(request, "orders/order/created.html", {"order": order})
+    else:
+        form = OrderCreateForm()
+    return render(request, "orders/order/create.html", {"cart": cart, "form": form})
