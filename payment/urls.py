bd5041f977a6830768c4b7a9e92e8adc55cc3bcb
holabayor <liasu.olabayo@gmail.com>
2023-06-04 22:19:12 +0100

    checkout

---
+++ payment/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("process/", views.payment_process, name="process"),
+    path("completed/", views.payment_completed, name="completed"),
+    path("canceled/", views.payment_canceled, name="canceled"),
+]
--- payment/views.py
+++ payment/views.py
@@ -45,3 +45,11 @@ def payment_process(request):
         return redirect(session.url, code=303)
     else:
         return render(request, "payment/process.html", locals())
+
+
+def payment_completed(request):
+    return render(request, "payment/completed.html")
+
+
+def payment_canceled(request):
+    return render(request, "payment/canceled.html")
