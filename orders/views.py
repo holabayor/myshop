4db0363cf192bc2b6b8bdd3d04554a247993ff42
holabayor <liasu.olabayo@gmail.com>
2023-06-08 19:24:58 +0100

    static files and orders pdf

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,15 @@
-from django.urls import reverse
-from django.shortcuts import render, redirect, get_object_or_404
+import weasyprint
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
-from .models import OrderItem, Order
-from .forms import OrderCreateForm
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+from django.urls import reverse
+
 from cart.cart import Cart
+
+from .forms import OrderCreateForm
+from .models import Order, OrderItem
 from .tasks import order_created
 
 
@@ -36,5 +42,17 @@ def order_create(request):
 
 @staff_member_required
 def admin_order_detail(request, order_id):
-    order = get_object_or_404(Order, order_id)
+    order = get_object_or_404(Order, id=order_id)
     return render(request, "admin/orders/order/detail.html", {"order": order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string("orders/order/pdf.html", {"order": order})
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f"filename=order_{order.id}.pdf"
+    weasyprint.HTML(string=html).write_pdf(
+        response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / "css/pdf.css")]
+    )
+    return response
